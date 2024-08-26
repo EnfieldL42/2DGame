a590f9f0accd9e494d4e935305ed7125ed1edd07
EnfieldL42 <possamai.bruno@gmail.com>
2024-08-27 11:51:32 +1200

    dd

--- Assets/Overcooked/Scripts/Player/newController.cs
+++ Assets/Overcooked/Scripts/Player/newController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -124,4 +124,4 @@ public class NewController : MonoBehaviour
     {
         input = context.ReadValue<Vector2>();
     }
-}
+}*/
